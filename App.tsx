fe629706f651bba1382a5ac3db8768d9008f69e5
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-01-23 15:36:11 +0800

    udpate

--- App.tsx
+++ App.tsx
@@ -15,28 +15,46 @@ import {
   useChatContext,
 } from 'react-native-chat-uikit';
 
+const appKey = 'easemob#easeim';
+const userId = 'du004';
+const userPs = '1';
+const peerId = 'du005';
+
 function SendMessage() {
-  // 0. Login
-  // 1. Send message
   const [page, setPage] = React.useState(0);
+  const [id, setId] = React.useState(userId);
+  const [ps, setPs] = React.useState(userPs);
+  const [peer, setPeer] = React.useState(peerId);
   const im = useChatContext();
-  const userId = 'du004';
-  const userPs = '1';
-  const peerId = 'du005';
 
   if (page === 0) {
     return (
       <SafeAreaView style={{flex: 1}}>
-        <TextInput placeholder="Please Login ID." />
-        <TextInput placeholder="Please Login token or password." />
+        <TextInput
+          placeholder="Please Login ID."
+          value={id}
+          onChangeText={setId}
+        />
+        <TextInput
+          placeholder="Please Login token or password."
+          value={ps}
+          onChangeText={setPs}
+        />
+        <TextInput
+          placeholder="Please peer ID."
+          value={peer}
+          onChangeText={setPeer}
+        />
         <Pressable
           onPress={() => {
+            console.log('test:zuoyu:login', id, ps);
             im.login({
-              userId: userId,
-              userToken: userPs,
+              userId: id,
+              userToken: ps,
               usePassword: true,
               result: res => {
                 console.log('login result', res);
+                console.log('test:zuoyu:error', res);
                 if (res.isOk === true) {
                   setPage(1);
                 }
@@ -59,7 +77,7 @@ function SendMessage() {
     return (
       <SafeAreaView style={{flex: 1}}>
         <ConversationDetail
-          convId={peerId}
+          convId={peer}
           convType={0}
           onBack={() => {
             setPage(0);
@@ -73,7 +91,6 @@ function SendMessage() {
 }
 
 function App(): React.JSX.Element {
-  const appKey = 'easemob#easeim';
   return (
     <Container options={{appKey: appKey}}>
       <SendMessage />
